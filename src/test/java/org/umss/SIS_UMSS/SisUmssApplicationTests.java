fe5390d122c928b2f88e93c724dd6f7283931e09
Nando1296 <nando1296@gmail.com>
2024-05-30 23:29:59 -0400

    Commit inicial

---
+++ src/test/java/org/umss/SIS_UMSS/SisUmssApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umss.SIS_UMSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisUmssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
