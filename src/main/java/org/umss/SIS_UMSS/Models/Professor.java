d152bbeb2432f8f9ed691a58ac327ab10ee7157a
Luis Fernando Cadima Campos <nando1296@gmail.com>
2024-06-07 20:45:26 -0400

    getter and setter professor

--- src/main/java/org/umss/SIS_UMSS/Models/Professor.java
+++ src/main/java/org/umss/SIS_UMSS/Models/Professor.java
@@ -27,4 +27,55 @@ public class Professor {
             inverseJoinColumns = @JoinColumn(name = "subject_id")
     )
     private List<Subject> subjects;
+
+    public Professor() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    public @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Email String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 }
